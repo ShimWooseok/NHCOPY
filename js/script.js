2d3aff37b01cfe6d56ccd4c2e6962099a7b3ae0d
sws <zaqplm7913@naver.com>
2022-04-28 21:49:34 +0900

    ss

--- js/script.js
+++ js/script.js
@@ -4,11 +4,32 @@ $(document).ready(function(){
     $(".global-menu").click(function(){
 
         $(".global-menu").toggleClass("active")
+
+
+
      
     })
+
+    $(".btn-top").click(function(){
+        // $("html , body").scrollTop(800)
+        $("html , body").animate({
+          "scrollTop": 0 ,
+        },300)
+      })
+
+
 
 
 
 
     // 끝
 })
+
+$(window).scroll(function(){
+    let scrollTop = $(this).scrollTop()
+    
+    console.log(scrollTop)
+    if(scrollTop>=200){
+      $(".btn-top").addClass("active")
+    }
+  })
